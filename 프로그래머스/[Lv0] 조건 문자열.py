a1faae271379081fa9e3b34f02a83a8f02884577
Park-Yuyeong <54975374+Park-Yuyeong@users.noreply.github.com>
2023-07-02 18:31:03 +0900

    eval 함수 활용

--- 프로그래머스/[Lv0] 조건 문자열.py
+++ 프로그래머스/[Lv0] 조건 문자열.py
@@ -1,11 +1,5 @@
 def solution(ineq, eq, n, m):
-    if eq == '=':
-        if ineq == '>':
-            return int(bool(n >= m))
-        else:
-            return int(bool(n <= m))
-
-    if ineq == '>':
-        return int(bool(n > m))
-    else:
-        return int(bool(n < m))
+    if eq == '!':
+        eq = ''
+        
+    return int(eval(str(n) + ineq + eq + str(m)))
