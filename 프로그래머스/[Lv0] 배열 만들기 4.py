e855315023147e4e1eb1f8ce0799eae30d89226d
Park-Yuyeong <54975374+Park-Yuyeong@users.noreply.github.com>
2023-07-02 20:33:15 +0900

    중복 코드 없도록 수정

--- 프로그래머스/[Lv0] 배열 만들기 4.py
+++ 프로그래머스/[Lv0] 배열 만들기 4.py
@@ -3,13 +3,10 @@ def solution(arr):
     i = 0
     
     while i < len(arr):
-        if not(stk):
-            stk.append(arr[i])
-            i += 1
-        elif stk[-1] < arr[i]:
-            stk.append(arr[i])
-            i += 1
-        else:
+        if stk and stk[-1] >= arr[i]:
             stk.pop()
-            
+        else:
+            stk.append(arr[i])
+            i += 1
+        
     return stk
