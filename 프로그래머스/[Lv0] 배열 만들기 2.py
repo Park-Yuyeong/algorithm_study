d0de105d56e6f058a36cde9b2140b806ae4098c0
Park-Yuyeong <54975374+Park-Yuyeong@users.noreply.github.com>
2023-07-02 19:31:54 +0900

    set 활용하여 코드 수정

--- 프로그래머스/[Lv0] 배열 만들기 2.py
+++ 프로그래머스/[Lv0] 배열 만들기 2.py
@@ -1,22 +1,7 @@
 def solution(l, r):
     answer = []
     for i in range(l, r+1):
-        if i % 5:
-            continue
-            
-        check = set(str(i))
-        isTrue = True
-        if len(check) > 2:
-            continue
-            
-        for c in check:
-            if c == "0" or c == "5":
-                pass
-            else:
-                isTrue = False
-                break
-                
-        if isTrue:
+        if not(set(str(i)) - set(['0', '5'])):
             answer.append(i)
                      
     return answer if len(answer) else [-1]
