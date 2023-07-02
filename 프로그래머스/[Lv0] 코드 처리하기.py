309259cebefaca8edc5f0a7bb790d4eece779d9c
Park-Yuyeong <54975374+Park-Yuyeong@users.noreply.github.com>
2023-07-02 18:54:58 +0900

    비트 연산자를 활용한 mode 변환

--- 프로그래머스/[Lv0] 코드 처리하기.py
+++ 프로그래머스/[Lv0] 코드 처리하기.py
@@ -3,19 +3,11 @@ def solution(code):
     ret = ''
     
     for idx in range(len(code)):
-        if mode == 0:
-            if code[idx] == '1':
-                mode = 1
-                continue
-            
-            if not(idx % 2):
-                ret += code[idx]
-        else:
-            if code[idx] == '1':
-                mode = 0
-                continue
-            
-            if idx % 2:
-                ret += code[idx]
+        if code[idx] == '1':
+            mode ^= 1 # 비트 연산자를 활용한 mode 변환
+            continue
+        
+        if idx % 2 == mode:
+            ret += code[idx]
     
     return ret if ret != '' else 'EMPTY'
