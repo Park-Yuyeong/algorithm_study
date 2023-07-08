dd27775d75b3606ef59be1806636b90a2985c788
Park-Yuyeong <54975374+Park-Yuyeong@users.noreply.github.com>
2023-07-08 15:46:49 +0900

    is_suffix 길이 값을 문자열 슬라이싱에 이용

--- 프로그래머스/[Lv0] 접미사인지 확인하기.py
+++ 프로그래머스/[Lv0] 접미사인지 확인하기.py
@@ -1,8 +1,2 @@
 def solution(my_string, is_suffix):
-    suffix = []
-    for i in range(len(my_string)):
-        suffix.append(my_string[i:])
-    
-    if is_suffix in suffix:
-        return 1
-    return 0
+    return int(my_string[-len(is_suffix):] == is_suffix)
