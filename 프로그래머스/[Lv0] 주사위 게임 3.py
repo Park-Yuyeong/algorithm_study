51c5999bba43fc5fb849f62934d474098bd3ec17
Park-Yuyeong <54975374+Park-Yuyeong@users.noreply.github.com>
2023-07-08 15:12:59 +0900

    주석 처리

--- 프로그래머스/[Lv0] 주사위 게임 3.py
+++ 프로그래머스/[Lv0] 주사위 게임 3.py
@@ -1,5 +1,5 @@
 def solution(a, b, c, d):
-    dice = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
+    dice = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0} # 주사위 표현에 dictionary 활용
     answer = 0
     diff = []
     
@@ -8,13 +8,12 @@ def solution(a, b, c, d):
     dice[c] += 1
     dice[d] += 1
     
-    max_value = max(dice.values())
-    
-    if len(set([a, b, c, d])) == 1:
+    # set() 함수 활용 - 나온 숫자 종류의 개수 확인
+    if len(set([a, b, c, d])) == 1: # 4 숫자가 모두 p로 같은 경우
         return 1111 * a
-    elif len(set([a, b, c, d])) == 4:
+    elif len(set([a, b, c, d])) == 4: # 4 숫자가 모두 다른 경우
         return min([a, b, c, d])
-    elif len(set([a, b, c, d])) == 3:
+    elif len(set([a, b, c, d])) == 3: # 4 숫자 중 2 숫자가 같고 나머지 숫자들은 다른 경우
         answer = 1
         for k, v in dice.items():
             if v == 1:
@@ -30,7 +29,10 @@ def solution(a, b, c, d):
             answer += k
             diff.append(k)
             
-    if max_value == 3:
+    max_value = max(dice.values())
+            
+    if max_value == 3: # 4 숫자 중 3 숫자가 같은 경우
         return answer**2
     
+    # 4 숫자가 두 개씩 값이 같은 경우
     return answer * abs(diff[0] - diff[1])
