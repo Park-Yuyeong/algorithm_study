53b9fe688d987b705c8c7162befe4e53bba70630
Park-Yuyeong <54975374+Park-Yuyeong@users.noreply.github.com>
2023-07-08 16:05:09 +0900

    슬라이싱 활용

--- 프로그래머스/[Lv0] 세로 읽기.py
+++ 프로그래머스/[Lv0] 세로 읽기.py
@@ -1,6 +1,2 @@
 def solution(my_string, m, c):
-    answer = ''
-    for i in range(len(my_string) // m):
-        x = i * m
-        answer += my_string[x + (c-1)]
-    return answer
+    return my_string[c-1::m]
