8a70d48b5b9d8262e2eb46a63eb0f3d5f2edab40
Park-Yuyeong <54975374+Park-Yuyeong@users.noreply.github.com>
2024-05-05 18:13:07 +0900

    Update [Lv1] 3진법 뒤집기.js

--- 프로그래머스_js/[Lv1] 3진법 뒤집기.js
+++ 프로그래머스_js/[Lv1] 3진법 뒤집기.js
@@ -1,18 +1,5 @@
 function solution(n) {
-    let n3 = '';
-    let answer = 0;
-    console.log(n.toString(3));
+    let answer = n.toString(3).split('').reverse().join('');
     
-    while (n !== 0) {
-        n3 += n % 3;
-        n = Math.floor(n / 3);
-    }
-
-    let l = n3.length;
-    
-    for (let i = 0; i < l; i++) {
-        answer += n3[i] * Math.pow(3, l - i - 1);
-    }
-    
-    return answer;
+    return parseInt(answer, 3);
 }
